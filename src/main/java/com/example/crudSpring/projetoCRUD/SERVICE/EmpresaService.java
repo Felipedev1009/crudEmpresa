42272d5b90666fab1a4f74bd826c910dbe45325a
Felipe <felipermdomingos@gmail.com>
2025-09-22 20:33:38 -0300

    (22-09-25)

--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,39 +1,53 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
- 
 import java.util.List;
-
+import java.util.Optional;
+ 
 import org.springframework.stereotype.Service;
-
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.REPOSITORY.EmpresaRepository;
  
 @Service
 public class EmpresaService {
-
-    //4 operações
-    //Selects, Inserts, Alter Table ou Delete
+private final EmpresaRepository empresaRepository;
  
-    //retorna todos os dados da empresa
-    //(*) = Tudo da tabela
-    //SELECT * FROM EMPRESA  (Select realiza listagem de dados)
-    //Select (Quais dados deseja listar) ([From]onde executar a tabela)
-    private final EmpresaRepository empresaRepository;
-
-    // método construtor da classe EmpresaService criando uma ligação com a classe Repository
-    public EmpresaService(EmpresaRepository ligaçãoEmpresaRepository){
-        empresaRepository = ligaçãoEmpresaRepository;
+    // metodo construtor da classe EmpresaSeevice
+    // criando uma ligação com a classe Repository
+   
+    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository){
+        empresaRepository = ligacaoEmpresaRepository;
     }
+//selects ou inserts ou alter table ou delete
+//select * From empresa (select realiza listagem de dados)
+//retorna todos os dados da empresa (* - tudo da tabela)
+public List<Empresa> findA11(){
+return empresaRepository.findAll();
+}//select * from empresa
  
-    public List<Empresa> findAll(){
-        return empresaRepository.findAll();
-    }
-
-    public Empresa cadastarEmpresa(Empresa dadosEmpresa){
-        return empresaRepository.save(dadosEmpresa);
-        }
-
-        public void deletarEmpresa(Empresa dadoEmpresa){
-            empresaRepository.delete(dadoEmpresa);
-        }
-
+public Empresa cadastarEmpresa(Empresa dadosEmpresa){
+return empresaRepository.save(dadosEmpresa);
 }
+ 
+public void deletarEmpresa(Empresa dadosEmpresa){
+    empresaRepository.delete(dadosEmpresa);
+}
+//realizar a busca de dados no banco usando  o id criado na classe
+ 
+public Optional <Empresa> buscarPorId(Long id){
+    return empresaRepository.findById(id);
+}
+ 
+public Empresa editarDadoEmpresa(Long id, Empresa dadosAtualizado){
+    Empresa empresaBuscada = buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Empresa não encontada"));
+    
+    empresaBuscada.setNome_empresa(dadosAtualizado.getNome_empresa());
+    empresaBuscada.setCnpj(dadosAtualizado.getCnpj());
+    empresaBuscada.setRamo(dadosAtualizado.getRamo()); // get, post, put, delete metodos 
+
+    return empresaRepository.save(empresaBuscada);
+}
+ public List<Empresa> buscarEmpresaporNome(String nome_empresa){
+    return empresaRepository.findByNome_empresaContainingIgnoreCase(nome_empresa);
+
+ }
+}
+ 
