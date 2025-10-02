184f5e21d8fc427e316e5a08b63b6e10c8dfd07c
Felipe <felipermdomingos@gmail.com>
2025-10-02 20:37:53 -0300

    (02-10-25)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
@@ -3,6 +3,8 @@ package com.example.crudSpring.projetoCRUD.CONTROLLER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
@@ -21,11 +23,26 @@ public class EmpresaController {
     public EmpresaController(EmpresaService ligacaoEmpresaService){
         this.empresaService = ligacaoEmpresaService;
     }
+
+    @GetMapping("/listarTodasEmpresas")
+    public String listarEmpresa(Model oModel) {
+        oModel.addAttribute("empresas", empresaService.findAll());
+        return "listarEmpresas";
+    }
+    
     
     @GetMapping("/viewCadEmpresa")
     public String mostrarFormCadrastro(Model model){
         model.addAttribute("empresa", new Empresa());
         return "cadastroEmpresa";
     }
+
+    @PostMapping("/salvarEmpresa")
+    public String salvarEmpresa(@ModelAttribute Empresa objempresa) {
+    //chamando o metodo cadastrar e passando o objeto ("pacotinho") com os dados que precisam ser salvos
+    empresaService.cadastarEmpresa(objempresa);
+
+    return "redirect:/empresaCTR/listarTodasEmpresas";
+}
 
 }
--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,53 +1,57 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
+
 import java.util.List;
 import java.util.Optional;
- 
+
 import org.springframework.stereotype.Service;
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.REPOSITORY.EmpresaRepository;
- 
+
 @Service
 public class EmpresaService {
-private final EmpresaRepository empresaRepository;
- 
+    private final EmpresaRepository empresaRepository;
+
     // metodo construtor da classe EmpresaSeevice
     // criando uma ligação com a classe Repository
-   
-    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository){
+
+    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository) {
         empresaRepository = ligacaoEmpresaRepository;
     }
-//selects ou inserts ou alter table ou delete
-//select * From empresa (select realiza listagem de dados)
-//retorna todos os dados da empresa (* - tudo da tabela)
-public List<Empresa> findA11(){
-return empresaRepository.findAll();
-}//select * from empresa
- 
-public Empresa cadastarEmpresa(Empresa dadosEmpresa){
-return empresaRepository.save(dadosEmpresa);
-}
- 
-public void deletarEmpresa(Empresa dadosEmpresa){
-    empresaRepository.delete(dadosEmpresa);
-}
-//realizar a busca de dados no banco usando  o id criado na classe
- 
-public Optional <Empresa> buscarPorId(Long id){
-    return empresaRepository.findById(id);
-}
- 
-public Empresa editarDadoEmpresa(Long id, Empresa dadosAtualizado){
-    Empresa empresaBuscada = buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Empresa não encontada"));
-    
-    empresaBuscada.setNome(dadosAtualizado.getNome());
-    empresaBuscada.setCnpj(dadosAtualizado.getCnpj());
-    empresaBuscada.setRamo(dadosAtualizado.getRamo()); // get, post, put, delete. metodos 
 
-    return empresaRepository.save(empresaBuscada);
-}
- public List<Empresa> buscarEmpresaporNome(String nome_empresa){
-    return empresaRepository.findByNomeContainingIgnoreCase(nome_empresa);
+    // selects ou inserts ou alter table ou delete
+    // select * From empresa (select realiza listagem de dados)
+    // retorna todos os dados da empresa (* - tudo da tabela)
+    public List<Empresa> findAll() {
+        return empresaRepository.findAll();
+    }// select * from empresa
 
- }
+    public Empresa cadastarEmpresa(Empresa dadosEmpresa) {
+        return empresaRepository.save(dadosEmpresa);
+    }
+
+    public void deletarEmpresa(Empresa dadosEmpresa) {
+        empresaRepository.delete(dadosEmpresa);
+    }
+    // realizar a busca de dados no banco usando o id criado na classe
+
+    public Optional<Empresa> buscarPorId(Long id) {
+        return empresaRepository.findById(id);
+    }
+
+    public Empresa editarDadoEmpresa(Long id, Empresa dadosAtualizado) {
+        Empresa empresaBuscada = buscarPorId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Empresa não encontada"));
+
+        empresaBuscada.setNome(dadosAtualizado.getNome());
+        empresaBuscada.setCnpj(dadosAtualizado.getCnpj());
+        empresaBuscada.setRamo(dadosAtualizado.getRamo()); // get, post, put, delete. metodos
+
+        return empresaRepository.save(empresaBuscada);
+    }
+
+    public List<Empresa> buscarEmpresaporNome(String nome_empresa) {
+        return empresaRepository.findByNomeContainingIgnoreCase(nome_empresa);
+
+    }
+
 }
- 
