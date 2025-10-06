0008573690a8262fdf79dc7d8ea3e6c2130db115
Felipe <felipermdomingos@gmail.com>
2025-10-06 20:35:08 -0300

    (06-10-25)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
@@ -11,6 +12,8 @@ import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -37,6 +40,8 @@ public class EmpresaController {
         return "cadastroEmpresa";
     }
 
+
+
     @PostMapping("/salvarEmpresa")
     public String salvarEmpresa(@ModelAttribute Empresa objempresa) {
     //chamando o metodo cadastrar e passando o objeto ("pacotinho") com os dados que precisam ser salvos
@@ -45,4 +50,23 @@ public class EmpresaController {
     return "redirect:/empresaCTR/listarTodasEmpresas";
 }
 
+@GetMapping("/editar/{id}")
+public String formEditar(@PathVariable("id") Long id, Model oModel) {
+    Empresa objEmpresa = empresaService.buscarPorId(id)
+        .orElseThrow(() -> new IllegalArgumentException("Empresa não encontrada"));
+ 
+    oModel.addAttribute("empresaEditar", objEmpresa);
+    return "editarEmpresa";
 }
+
+@PostMapping("/atualizarEmpresa") // serve para quando quiser cadastrar algo ou mudar no banco de dados
+public String atualizarEmpresa(@PathVariable Long id, @ModelAttribute Empresa objEmpresaAtualizado) {
+
+    return "redirect:/empresaCTR/listarTodasEmpresas";
+}
+
+
+}
+
+
+
