2216e2752c4f968860e63f485da62e8dff3b16ab
Felipe <felipermdomingos@gmail.com>
2025-11-13 20:47:13 -0300

    (13/11)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
@@ -1,15 +1,20 @@
 package com.example.crudSpring.projetoCRUD.CONTROLLER;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Sse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.crudSpring.projetoCRUD.ENTITY.Funcionario;
 import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
 import com.example.crudSpring.projetoCRUD.SERVICE.FuncionarioService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -26,6 +31,8 @@ public class FuncionarioController {
     public String listarTodosFuncionario(Model oModel) {
         oModel.addAttribute("funcionarios", ligacaoFuncionarioService.listarTodosFuncionarios());
         
+        oModel.addAttribute("empresa", ligacaoEmpresaService.findAll());
+        
         return "listarFuncionarios";
     }
     
@@ -33,9 +40,20 @@ public class FuncionarioController {
     public String mostrarFormCadastro(Model oModel) {
 
         oModel.addAttribute("funcionario", new Funcionario());
+        
         oModel.addAttribute("empresas", ligacaoEmpresaService.findAll());
 
         return "cadastrarFuncionario";
     }
+
+    @PostMapping("/salvarFuncionario")
+    public String cadastrarFuncionario(@ModelAttribute Funcionario objFuncionario){
+
+    ligacaoFuncionarioService.cadastFuncionario(objFuncionario);
+
+    return "redirect:/funcionarioCTR/listarFunc";
+
+    }
+    
     
 }
